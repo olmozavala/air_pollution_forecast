a221dbb39042337ab2c694ca24001bab16ca346c
PA-Segura <segurachavez@gmail.com>
2024-01-31 18:38:02 -0600

    Update 7_Operativo script

--- 7_Operativo_old.py
+++ 7_Operativo_old.py
@@ -1,77 +1,87 @@
-# %%
-# Standard imports
-import os
+# %% trusted=true
 import sys
-from copy import deepcopy
-from datetime import datetime, timedelta
-
-# 3rd-p imports
+import psycopg2
+from conf.MakeWRF_and_DB_CSV_UserConfiguration import getPreprocDBParams, getPreprocWRFParams
+from db.queries import getPollutantFromDateRange, getAllStations
+from db.sqlCont import getPostgresConn
+from proj_preproc.wrf import crop_variables_xr, subsampleData
+import xarray as xr
+from datetime import date, datetime, timedelta
+from sklearn import preprocessing
+from conf.localConstants import constants
+from pandas import DataFrame
+import pandas as pd
+import time
+from ai_common.constants.AI_params import *
+from os.path import join
+from conf.params import LocalTrainingParams, PreprocParams
+from conf.TrainingUserConfiguration import get_makeprediction_config
+from AI.data_generation.utilsDataFormat import *
+from ai_common.models.modelSelector import select_1d_model
+from os import listdir
+import glob
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import xarray as xr
-import cartopy.crs as ccrs
-
-# Local imports
-from ai_common.constants.AI_params import *
-from ai_common.models.modelSelector import select_1d_model
-from conf.MakeWRF_and_DB_CSV_OperativoConfiguration import getPreprocWRFParams
-from conf.TrainingOperativoConfiguration import get_makeprediction_config
-from conf.params import PreprocParams
-from db.queries import getPollutantFromDateRange
-from db.sqlCont import getPostgresConn
-from os.path import join
-from pandas import DataFrame
-from proj_io.inout import filter_data, add_previous_hours, generateDateColumns, get_column_names, get_month_folder_esp
+from matplotlib.colors import LogNorm
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error, r2_score
+from copy import deepcopy
+from proj_io.inout import filter_data, add_previous_hours, add_forecasted_hours, generateDateColumns, get_column_names, read_wrf_files_names, get_month_folder_esp, save_columns
 from proj_preproc.preproc import loadScaler
-from proj_preproc.wrf import crop_variables_xr, subsampleData
-from AI.data_generation.utilsDataFormat import *
+from proj_prediction.prediction import compile_scaler
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.io.shapereader import Reader
+from cartopy.feature import ShapelyFeature
 
+
+# sys.path.append('./eoas_pyutils')  # Doesn't work when using a conda env outside home
+sys.path.append('/home/olmozavala/air_pollution_forecast/eoas_pyutils')
+from viz.figure_generator import plot_input_output_data
+from viz_utils.eoa_viz import EOAImageVisualizer
+from io_utils.io_common import  create_folder
 import warnings
-
 # Suppress the specific PerformanceWarning from pandas
 warnings.filterwarnings('ignore', category=pd.errors.PerformanceWarning)
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-# Paths for model processing and predictions 
-
-# csv_xcols_path = '/ZION/AirPollutionData/Data/TrainingTestsPS/test01_15x3_5btsp_24ph_v20_2010_2019/X_columns.csv'
-# model_weights_file = '/ZION/AirPollutionData/Data/TrainingTestsPS/test01_15x3_5btsp_24ph_v20_2010_2019/models/TestsPS_4paper_otres_2023_10_26_20_45-epoch-47-loss-0.25776392.hdf5'
-# file_name_norm = "/ZION/AirPollutionData/Data/TrainingTestsPS/test01_15x3_5btsp_24ph_v20_2010_2019/norm/TestsPS_4paper_otres_2023_10_26_20_39_scaler.pkl"
-
-csv_xcols_path = './operativo_files/test01_15x3_5btsp_24ph_v20_2010_2019/X_columns.csv'
-model_weights_file = './operativo_files/test01_15x3_5btsp_24ph_v20_2010_2019/models/TestsPS_4paper_otres_2023_10_26_20_45-epoch-47-loss-0.25776392.hdf5'
-file_name_norm = "./operativo_files/test01_15x3_5btsp_24ph_v20_2010_2019/norm/TestsPS_4paper_otres_2023_10_26_20_39_scaler.pkl"
-
-wrf_input_folder = "/ServerData/WRF_2017_Kraken"
-
-# Fixing parms used for training
-cur_pollutant = "otres"
-all_stations = ["UIZ","AJU" ,"ATI" ,"CUA" ,"SFE" ,"SAG" ,"CUT" ,"PED" ,"TAH" ,"GAM" ,"IZT" ,"CCA" ,"HGM" ,"LPR" ,
-                    "MGH" ,"CAM" ,"FAC" ,"TLA" ,"MER" ,"XAL" ,"LLA" ,"TLI" ,"UAX" ,"BJU" ,"MPA" ,
-                    "MON" ,"NEZ" ,"INN" ,"AJM" ,"VIF"]
-                    
-hours_before = 24  # Hours previous from pollution info
-forecasted_hours = 24  # Forecasted hours from 1 to 24
-grid_size_wrf = 4  # 4 for 4x4
-
-# *********** Reads configfiles parameters ***********
-config = get_makeprediction_config()
-wrf_config = getPreprocWRFParams()
-meteo_var_names = wrf_config[PreprocParams.variables]    
-bbox = wrf_config[PreprocParams.bbox]
-times = wrf_config[PreprocParams.times]
-
-# Cargar el CSV con los nombres de las columnas 
+# Cargar el CSV con los nombres de las columnas
+csv_xcols_path = '/ZION/AirPollutionData/Data/TrainingTestsPS/4paper_test01_15x3_5btsp_24ph_v20_2010_2019/X_columns.csv'
 column_names_df = pd.read_csv(csv_xcols_path, header=None)
 column_names = column_names_df.iloc[:, 0].tolist()
 column_names.remove('0')
 
-# %% Operativo function
+#forecast_time = datetime(2023, 1, 12, 14, 0, 0, 0)
+#forecast_time = datetime.now().replace(minute=0, second=0, microsecond=0)
+# %%  Read time from command line
+
 def operativo(forecast_time):
-    # %% Creat an empty dataframe with proper column names
+    # %% trusted=true
+    config = get_makeprediction_config()
+    # *********** Reads the parameters ***********
+    model_weights_file = '/ZION/AirPollutionData/Data/TrainingTestsPS/test01_15x3_5btsp_24ph_v20_2010_2019/models/TestsPS_4paper_otres_2023_10_26_20_45-epoch-47-loss-0.25776392.hdf5'
+    file_name_norm = file_name_norm = "/ZION/AirPollutionData/Data/TrainingTestsPS/test01_15x3_5btsp_24ph_v20_2010_2019/norm/TestsPS_4paper_otres_2023_10_26_20_39_scaler.pkl"
+    cur_pollutant = "otres"
+    hours_before = 24  # Hours previous from pollution info
+    forecasted_hours = 24  # Forecasted hours from 1 to 24
+    grid_size_wrf = 4  # 4 for 4x4
+
+    wrf_config = getPreprocWRFParams()
+    meteo_var_names = wrf_config[PreprocParams.variables]
+    wrf_input_folder = "/ServerData/WRF_2017_Kraken"
+    bbox = wrf_config[PreprocParams.bbox]
+    times = wrf_config[PreprocParams.times]
+    # print(bbox)
+
+    all_stations = ["UIZ","AJU" ,"ATI" ,"CUA" ,"SFE" ,"SAG" ,"CUT" ,"PED" ,"TAH" ,"GAM" ,"IZT" ,"CCA" ,"HGM" ,"LPR" ,
+                    "MGH" ,"CAM" ,"FAC" ,"TLA" ,"MER" ,"XAL" ,"LLA" ,"TLI" ,"UAX" ,"BJU" ,"MPA" ,
+                    "MON" ,"NEZ" ,"INN" ,"AJM" ,"VIF"]
+
+    # %% Creat an empty dataframe with proper column names trusted=true
     contaminant_columns = [f'cont_{cur_pollutant}_{station}' for station in all_stations]
     prev_contaminant_columns = [f'minus_{hour+1:02d}_{x}' for hour in range(hours_before)  for x in contaminant_columns]
     all_meteo_columns = [f'{meteo_var}_{loc}_h{hour}' for hour in range(0, forecasted_hours) for meteo_var in meteo_var_names for loc in range(16) ]
@@ -87,11 +97,11 @@ def operativo(forecast_time):
     date_format = '%Y-%m-%d:%H'
     timestamp_format = '%Y-%m-%d %H:%M:%S'
 
-    # %%
+    # %% trusted=true
     print(f"We will try to forecast time: {forecast_time.strftime(timestamp_format)}")
-    # %%
+# %%
     try:
-        # %% --------------------- Read from database (current values and previous ones)
+        # %% --------------------- Read from database (current values and previous ones) trusted=true
         start_datetime_df = forecast_time - timedelta(hours=hours_before+3)  # We are adding these 3 just for safety
         print(F"Getting data from {start_datetime_df.strftime(date_format)} to {forecast_time.strftime(date_format)}")
 
@@ -109,6 +119,8 @@ def operativo(forecast_time):
 
         # Define an empty X input to our model (we could validate all the sizes are correct here)
         X = DataFrame(index=[forecast_time], columns=contaminant_columns+all_time_colums+all_meteo_columns+prev_contaminant_columns)
+        # Saving the columsn should be in order
+        # save_columns(X, 'X_columns.csv')
 
         # Create empty dataframe with proper dimensions
         if len(dates) < hours_before:
@@ -117,7 +129,7 @@ def operativo(forecast_time):
             raise Exception(f"Not enough dates were found len(dates)={len(dates)}")
 
         print("Done!")
-        # %%
+        # %% trusted=true
         # Template dataframe
         print("Making empty dataset...")
         df = DataFrame(index=dates[0:hours_before], columns=[f'cont_{cur_pollutant}_{station}' for station in all_stations])
@@ -131,25 +143,25 @@ def operativo(forecast_time):
         # print(df.head())
         print("Done!")
 
-        # %%
+        # %% trusted=true
         print("Adding prevoius hours...")
         df_shifted = add_previous_hours(df, hours_before=hours_before)
         # print(df_shifted.head())
         print("Done!")
 
-        # %%
+        # %% trusted=true
         print("Updating our template dataframe with pollution data...")
         print(f"(Before) Number of not nan values in input data {X.count().sum()}/{X.shape[1]}")
         X.update(df_shifted)
         print(f"(After) Number of not nan values in input data {X.count().sum()}/{X.shape[1]}")
         print("Done!")
-        # %% Add time columns here
+        # %% Add time columns here trusted=true
         print("Adding time columns...")
         time_cols, time_values = generateDateColumns(datetimes=[forecast_time])
         X.update(DataFrame(index=[forecast_time], columns=time_cols, data=np.array([x[0] for x in time_values]).reshape(1,len(time_values))))
         print(f"(After) Number of not nan values in input data {X.count().sum()}/{X.shape[1]}")
 
-        # %% ----------------------- Read from WRF forecast (current values and next ones)
+        # %% ----------------------- Read from WRF forecast (current values and next ones) trusted=true
         print("Adding meteorological data...")
         gmt_correction = -6 # con -6 se obtiene start_datetime_wrf  más 6 horas que sería la hora en UTC. porlo que parece -6 ciertamente corrige.
         start_datetime_wrf = forecast_time - timedelta(hours=gmt_correction)  # We are adding these 3 just for safety 
@@ -166,8 +178,29 @@ def operativo(forecast_time):
 
         cur_xr_ds = xr.open_dataset(wrf_file, decode_times=False)
         cropped_xr_ds, newLAT, newLon = crop_variables_xr(cur_xr_ds, meteo_var_names, bbox, times=times)
+        # cropped_xr_ds= cur_xr_ds.sel(XLAT=slice(bbox[0], bbox[1]), XLONG=slice(bbox[2], bbox[3]))
+        # newLAT  = cropped_xr_ds['XLAT'].values
+        # newLon  = cropped_xr_ds['XLONG'].values
 
-# %%
+        # # %% Plot using caartopy show country limits
+        # bbox = [newLon[0], newLon[-1], newLAT[0], newLAT[-1]]
+        # fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw={'projection': ccrs.PlateCarree()})
+        # ax.set_extent(bbox)
+        # ax.add_feature(cfeature.COASTLINE)
+        # shape_feature = ShapelyFeature(Reader("data/MEX_adm1.shp").geometries(), ccrs.PlateCarree(), 
+        # edgecolor='black', facecolor='none')
+        # ax.add_feature(shape_feature)
+        # im = ax.imshow(cropped_xr_ds['T2'].values[0,:,:], extent=bbox, origin='lower', transform=ccrs.PlateCarree(), cmap='coolwarm')
+        # gl = ax.gridlines(draw_labels=True, color='grey', alpha=0.5, linestyle='--')
+        # ax.add_feature(shape_feature, edgecolor='black', facecolor='none')
+        # gl.top_labels = False
+        # gl.left_labels = False
+        # gl.xlabel_style = {'size': 10, 'weight':'bold'}
+        # # plt.colorbar(im, location='right', shrink=.6, pad=.12)
+        # plt.show()
+
+# %% trusted=true
+
         # %% Subsampling the data
         subsampled_xr_ds, coarselat, coarselon = subsampleData(cropped_xr_ds, meteo_var_names, grid_size_wrf, grid_size_wrf)
         wrf_time_format = '%Y-%m-%d_%H:%M:%S'
@@ -203,26 +236,24 @@ def operativo(forecast_time):
         # print(f"(After) Number of not nan values in input data {X.count().sum()}/{X.shape[1]}")
 
         # ------------------------------ FINALLY WE HAVE FULL INPUT X -----------------------------------
-
-        # %% type by columns orignal the all Getting ======
+        
+        # %% type by columns orignal the all Getting ====== trusted=true
         all_contaminant_columns, all_meteo_columns, all_time_colums = get_column_names(X)
         print(f"Number of columns total: {X.shape[1]} contaminant: {len(all_contaminant_columns)} Meteo: {len(all_meteo_columns)} Time:{len(all_time_colums)}")
 
-        # %% pollutants other for columns Remove ======
+        # %% pollutants other for columns Remove ====== trusted=true
         print("Filtering single pollutant..")
         X_filt = filter_data(X, filter_type='single_pollutant', filtered_pollutant=cur_pollutant) 
         all_contaminant_columns, all_meteo_columns, all_time_colums = get_column_names(X_filt)
         print(f"(After) Number of not nan values in input data {X_filt.count().sum()}/{X_filt.shape[1]}")
         print(f"(After) contaminant: {len(all_contaminant_columns)} Meteo: {len(all_meteo_columns)} Time:{len(all_time_colums)}")
-
-        # %% ------ reordering X_filt
+        # %% ------ reordering X_filt        
+        # Suponiendo que 'X' es tu DataFrame existente
         ################
         # Ordenar las columnas de 'X' para que coincidan con el orden en el CSV
-
         X_filt = X_filt[column_names]
-
         ################
-        # %% -------- Normalizing data
+        # %% -------- Normalizing data trusted=true
         # loading of original scaler object
         scaler_orig = loadScaler(file_name_norm)
 
@@ -278,16 +309,16 @@ def operativo(forecast_time):
 
         print(F'X {X_norm.shape}, Memory usage: {X_norm.memory_usage().sum()/1024**2:02f} MB')
 
-        # %%
+        # %% trusted=true
         print("Removing time index...")
         X_norm.reset_index(drop=True, inplace=True)
 
-        # %% Replace all the nan values with another value
+        # %% Replace all the nan values with another value trusted=true
         replace_value = 0
         print(f"Replacing nan values with {replace_value}...")
         X_norm.fillna(replace_value, inplace=True)
 
-        # %% ******************* Selecting the model **********************
+        # %% ******************* Selecting the model ********************** trusted=true
         config[ModelParams.INPUT_SIZE] = X_norm.shape[1]
         config[ModelParams.NUMBER_OF_OUTPUT_CLASSES] = len(all_stations)*forecasted_hours
 
@@ -299,18 +330,18 @@ def operativo(forecast_time):
         model.load_weights(model_weights_file)
         print("Done!")
 
-        # %% Calculo de predicciones de el dataset de test:
+        # %% Calculo de predicciones de el dataset de test: trusted=true
         print('Making prediction ....')
         Y_pred = model.predict(X_norm.values)
         print("Done!")
 
-# %% A function is defined to generate custom scaler objects
+# %% A function is defined to generate custom scaler objects trusted=true
         # %%
         print("Denormalizing...")
         scaler_y = generate_scaler(scaler,y_cols)
         print("Done ...")
 
-        # %% Desescalar las predicciones
+        # %% Desescalar las predicciones trusted=true
         Y_pred_descaled = scaler_y.inverse_transform(Y_pred)
 
         # Convertir Y_pred_descaled en un DataFrame de pandas
@@ -318,7 +349,7 @@ def operativo(forecast_time):
         # Verificar el DataFrame
         print(y_pred_descaled_df.head())
 
-        # %% Plot results
+        # %% Plot results trusted=true
         t = 30
         fig, axs = plt.subplots(1, 1, figsize=(10, 10))
 
@@ -334,8 +365,8 @@ def operativo(forecast_time):
         plt.legend()
         plt.title(f"Forecast for all stations at time {forecast_time.strftime(date_format)}")
         plt.show()
-# %%
-        # %%
+# %% trusted=true
+        #%%
         all_hours = ','.join([f'hour_p{x:02d}' for x in range(1,25)])
         # print(all_hours)
 
@@ -360,33 +391,35 @@ def operativo(forecast_time):
             finally:
                 conn.close()
             print("Done")
-    # %%
+    #%%
     except Exception as e:
         print(f"Error: Failed for {forecast_time.strftime(date_format)} with error {e}")
 
-# %% --------- Running operativo
+# %% --------- Running the operativo trusted=true
+import time
+
+def waiting_with_updates(total_minutes, update_interval):
+    """funcion que pausa, e indica cuanto falta por esperar
+    total_minutes and update_interval in minutes
+    i.e. waiting_with_updates(60, 10)"""
+
+    for remaining in range(total_minutes, 0, -update_interval):
+        print(f'Waiting for {remaining} minutes...')
+        time.sleep(update_interval * 60)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        # Si se proporciona un argumento, usarlo
-        try:
-            forecast_time = datetime.strptime(sys.argv[1], "%Y-%m-%d %H:%M")
-        except ValueError:
-            print("Incorrect date format. Please use YYYY-MM-DD HH:MM")
-            exit(1)
-    else:
-        # Usar la hora actual si no se proporciona un argumento
+    while True:
         forecast_time = datetime.now().replace(minute=0, second=0, microsecond=0)
-
-    print(forecast_time)
-    try:
+        print(forecast_time)
         operativo(forecast_time)
-        exit(0)
-    except Exception as e:
-        print(f"Error during operation: {e}")
-        exit(1)
+        waiting_with_updates(60, 10)
+    
+    #forecast_time  = datetime.strptime("2023-07-26:3", date_format)
+    #forecasted_times = pd.date_range(start='2022-01-01T0', end='2023-12-07T16', freq='H')
+    #forecasted_times = pd.date_range(start='2023-01-31T00', end='2023-12-07T16', freq='H')
+    #forecasted_times = pd.date_range(start='2023-09-10T00', end='2023-12-09T23', freq='H')    
+    #for c_forecast_time in forecasted_times:
+    #    operativo(c_forecast_time)
 
-
-# Ejemplo de llamadas desde terminal:
-# python 7_Operativo_refactor.py # > # date "+%Y-%m-%d %H:%M" | python 7_Operativo_refactor.py
-# python 7_Operativo_refactor.py "2023-12-14 15:00"
+#%%
